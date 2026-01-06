fc3ee3028a424dc0ffb8fdbe92226feefb8fc269
Liam Lethbridge <69471164+MrClever101@users.noreply.github.com>
2026-01-06 18:30:18 +0000

    made the file more consistent and added prior-posterior function

--- src/GaussianProcess.py
+++ src/GaussianProcess.py
@@ -7,12 +7,12 @@ import matplotlib.pyplot as plt
 
 
 class GaussianProcess():
-    def __init__(self, kernelFunction: KernelFunction, variance = 1):
+    def __init__(self, kernelFunction: KernelFunction, noise_variance = 1):
         self.kernelFunction = kernelFunction
-        self.x = np.atleast_1d(0)
-        self.y = np.atleast_1d(0)
-        self.kAA = np.atleast_1d(variance)
-        self.variance = variance
+        self.x = None
+        self.y = None
+        self.kAA = np.atleast_1d(noise_variance)
+        self.noise_variance = noise_variance
         
 
     def observeData(self, x: Union[int, np.ndarray], y: Union[int, np.ndarray]):
@@ -21,13 +21,19 @@ class GaussianProcess():
         self.kAA = self.kernelFunction(x)
 
     def inference(self, x: Union[int, np.ndarray]):
-        np.atleast_1d(x)
+        x = np.atleast_1d(x)
         kAB = self.kernelFunction(self.x, x)
         kBB = self.kernelFunction(x)
-        kAAp = self.kAA + np.identity(len(self.kAA))*self.variance
+        kAAp = self.kAA + np.eye(len(self.x))*self.noise_variance
 
-        mu = np.matmul(kAB,np.matmul(np.linalg.inv(kAAp),self.y))
-        sigma = kBB - np.matmul(kAB,np.matmul(np.linalg.inv(kAAp),np.matrix.transpose(kAB)))
+        L = np.linalg.cholesky(kAAp)
+
+        alpha = np.linalg.solve(L.T, np.linalg.solve(L, self.y))
+
+        mu = kAB @ alpha
+
+        v = np.linalg.solve(L, kAB.T)
+        sigma = kBB - v.T @ v
 
         return mu, sigma
 
@@ -41,11 +47,11 @@ class GaussianProcess():
         plt.plot(self.x, means)
         plt.fill_between(self.x, means-np.diagonal(variances), means+np.diagonal(variances), alpha = 0.1, color = "b")
         plt.savefig("figures/fitVis.png")
-
-
+        
+    
 
 if __name__ == "__main__":
-    gp = GaussianProcess(RadialBasisFunction(sigma=1, lengthscale=10), variance=1)
+    gp = GaussianProcess(RadialBasisFunction(sigma=1, lengthscale=10))
     x = np.linspace(0,20, 1000)
     y = generateSinData(x, sigma=0.5)
     gp.observeData(x,y)
--- src/visualisation.py
+++ src/visualisation.py
@@ -1,32 +1,38 @@
-from kernelFuncts import RadialBasisFunction, KernelFunction, RationalQuadraticFunction, PeriodicFunction, CompositeKernel, LinearFunction
+from src.kernelFuncts import RadialBasisFunction, KernelFunction, RationalQuadraticFunction, PeriodicFunction, CompositeKernel, LinearFunction
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+from src.GaussianProcess import GaussianProcess
+from src.data import generateLinearData, generateSinData
+
+alphabet = "abcdefghi"
 
 def visualiseKernel(kernel:KernelFunction):
     """This function visualises the kernel."""
     matrixOfValues = kernel(np.linspace(0,5, 50))
-    values = kernel(np.array([0]), np.linspace(-5,5,100))
+    values = kernel(np.array([0]), np.linspace(-5,5,1000))
 
-    fig, axes = plt.subplots(1,2, width_ratios=[1,1], height_ratios=[1])
+    fig, axes = plt.subplots(1,2, width_ratios=[1,1], height_ratios=[1], figsize = (20,8))
 
     # first axis is graph of values
-    axes[0].plot(np.linspace(-5,5,100), values)
+    axes[0].plot(np.linspace(-5,5,1000), values)
     axes[0].set_box_aspect(1)
-    axes[0].set_xlabel("(x-y)")
-    axes[0].set_ylabel("Kernel output")
+    axes[0].set_xlabel("(x-y)", fontsize = 20, fontfamily = "serif")
+    axes[0].set_ylabel("Kernel output", fontsize = 20, fontfamily = "serif")
     
     # second axis is visual pattern
-    im = axes[1].imshow(matrixOfValues)
+    im = axes[1].imshow(matrixOfValues, cmap = "magma", vmin = 0, vmax = 1)
     axes[1].set_xticks([])
     axes[1].set_yticks([])
     divider = make_axes_locatable(axes[1])
     cax = divider.append_axes('right', size='5%', pad=0.05)
-    fig.colorbar(im, cax=cax, orientation='vertical')
+    cbar = fig.colorbar(im, cax=cax, orientation='vertical')
+    cbar.ax.tick_params(labelsize=20) 
+    for index, axis in enumerate(axes):
+        fixTickParams(axis, index)
 
-    # fig.suptitle(f"σ = {kernel.sigma} Lengthscsale = {kernel.lengthscale}", fontsize=16)
     fig.tight_layout()
-    fig.savefig(f"figures/{kernel.kernType}.png")
+    fig.savefig(f"figures/{kernel.kernType.replace(" ", "")}.png")
     # plt.show()
 
 
@@ -39,28 +45,30 @@ def showAll():
     comp.kernType = "Locally Periodic"
     kernels = [rbf, rq, p, comp]
 
-    fig, axes = plt.subplots(2, len(kernels), figsize = (18,8))
+    fig, axes = plt.subplots(2, len(kernels), figsize = (20,8))
     for i, kernel in enumerate(kernels):
         matrixOfValues = kernel(np.linspace(0,5, 50))
         values = kernel(np.array([0]), np.linspace(-5,5,100))
         axes[0][i].plot(np.linspace(-5,5,100), values)
         axes[0][i].set_box_aspect(1)
-        axes[0][i].set_xlabel("(x - y)", fontsize = 12, fontfamily = "serif")
+        # axes[0][i].set_xlabel("(x - y)", fontsize = 12, fontfamily = "serif")
 
         
         # second axis is visual pattern
-        im = axes[1][i].imshow(matrixOfValues)
+        im = axes[1][i].imshow(matrixOfValues, cmap = "magma", vmin = 0, vmax = 1)
         axes[1][i].set_xticks([])
         axes[1][i].set_yticks([])
         divider = make_axes_locatable(axes[1][i])
         cax = divider.append_axes('right', size='5%', pad=0.05)
         fig.colorbar(im, cax=cax, orientation='vertical')
 
-        axes[0][i].set_title(kernel.kernType, fontsize = 18, fontfamily = "serif")
 
     for i in range(1,len(kernels)):
         axes[0][i].set_yticks([])
     axes[0][0].set_ylabel("Kernel output", fontsize = 12, fontfamily = "serif")
+
+    for index, axis in enumerate(axes.flatten()):
+        fixTickParams(axis, index)
 
     fig.savefig("figures/Comparisons.png")    
 
@@ -69,7 +77,7 @@ def testHypes():
     
     kernels = [RadialBasisFunction(1,1), RadialBasisFunction(1,2), RadialBasisFunction(0.75,1), RadialBasisFunction(0.75,2)]
 
-    fig, axes = plt.subplots(2, len(kernels), figsize = (18,8))
+    fig, axes = plt.subplots(2, len(kernels), figsize = (20,8))
     for i, kernel in enumerate(kernels):
         matrixOfValues = kernel(np.linspace(0,5, 50))
         values = kernel(np.array([0]), np.linspace(-5,5,100))
@@ -86,20 +94,94 @@ def testHypes():
         cax = divider.append_axes('right', size='5%', pad=0.05)
         fig.colorbar(im, cax=cax, orientation='vertical')
 
-        axes[0][i].set_title(f"Sigma: {kernel.sigma}, lengthscale: {kernel.lengthscale}", fontsize = 15, fontfamily = "serif")
 
     for i in range(1,len(kernels)):
         axes[0][i].set_yticks([])
     axes[0][0].set_ylabel("Kernel output", fontsize = 12, fontfamily = "serif")
-
+    for index, axis in enumerate(axes.flatten()):
+        fixTickParams(axis, index)
     fig.savefig("figures/hyperparams.png")   
 
+def showGPLengthscales():
+    fig, axes = plt.subplots(1,3, figsize = (20,8))
+    x = np.linspace(0,20,250)
+    y = generateSinData(x, 5, 0.1)
+    gps = [GaussianProcess(RadialBasisFunction(lengthscale=0.1)), GaussianProcess(RadialBasisFunction(lengthscale=1)), GaussianProcess(RadialBasisFunction(lengthscale=10))]
+    for i in range(3):
+        gps[i].observeData(x,y)
+        means, variances = gps[i].inference(x)
+        axes[i].scatter(x,y, s=4, marker="x", c="black")
+        axes[i].plot(x, means)
+        axes[i].fill_between(x, means-np.diagonal(variances)*2, means+np.diagonal(variances)*2, alpha = 0.1, color = "b")
+        axes[i].set_xticks([])
+        axes[i].set_yticks([])
+    for index, axis in enumerate(axes):
+        fixTickParams(axis, index)
+    fig.tight_layout()
+    fig.savefig("figures/lengthscales.png")
+
+
+def priorDistSample(gp: GaussianProcess, nsamples: int = 3):
+    """This function plots and samples functions from the means and covariance.
+    Args:
+        gp: the gaussian process having observed data.
+    """
+
+    fig, axes = plt.subplots(ncols=2, figsize = (20,10), sharey=True)
+    
+    # calculate the mean and covariance for prior.
+    sigma = gp.noise_variance
+    
+    kernel = gp.kernelFunction
+    x = np.linspace(0,100,100)
+    means = np.zeros(100)
+    kernelMatrix = kernel(x)
+    axes[0].plot(x,means)
+    axes[0].fill_between(x, means-kernelMatrix.diagonal(), means+kernelMatrix.diagonal(), alpha = 0.1)
+    postMeans,PostVariance = gp.inference(x)
+    axes[1].plot(x,postMeans, label="Mean function")
+    axes[1].fill_between(x, postMeans-PostVariance.diagonal(), postMeans+PostVariance.diagonal(), alpha = 0.1, label="Variance range")
+    axes[1].scatter(gp.x, gp.y, marker = "x", c="black", s=40, label="Observed data")
+    f = np.random.multivariate_normal(means, kernelMatrix)
+    axes[0].plot(x, f, linestyle = "dashed", color = "black", linewidth = 1, label="Sample")
+    f = np.random.multivariate_normal(postMeans, PostVariance)
+    axes[1].plot(x, f, linestyle = "dashed", color = "black", linewidth = 1)
+
+    for i in range(nsamples-1):
+        f = np.random.multivariate_normal(means, kernelMatrix)
+        axes[0].plot(x, f, linestyle = "dashed", color = "black", linewidth = 1)
+        f = np.random.multivariate_normal(postMeans, PostVariance)
+        axes[1].plot(x, f, linestyle = "dashed", color = "black", linewidth = 1)
+
+    # fig.legend(loc="center right")
+    for index, axis in enumerate(axes):
+        fixTickParams(axis, index)
+    fig.savefig("figures/priorVsPosterior.png")
+
+
+    
+def fixTickParams(axis, index):
+    """This function fixes axis for consistency."""
+    axis.tick_params(direction = "in", labelsize=15)
+    axis.set_title(f"({alphabet[index]})", size = 30, fontfamily = "serif", fontweight = "bold")
+
+    
+
+
 if __name__ == "__main__":
-    # rbf = RadialBasisFunction(lengthscale=5)
-    # rq = RationalQuadraticFunction(alpha=1)
-    # p = PeriodicFunction()
-    # lin = LinearFunction()
-    # comp = CompositeKernel(p, rbf, "x")
-    # visualiseKernel(lin)
-    # showAll()
+    rbf = RadialBasisFunction(lengthscale=1)
+    rq = RationalQuadraticFunction(alpha=1)
+    p = PeriodicFunction()
+    lin = LinearFunction()
+    comp = CompositeKernel(p, RadialBasisFunction(lengthscale=4), "x")
+    visualiseKernel(rbf)
+    visualiseKernel(rq)
+    visualiseKernel(p)
+    visualiseKernel(lin)
+    visualiseKernel(comp)
+    showAll()
     testHypes()
+    showGPLengthscales()
+    gp = GaussianProcess(RadialBasisFunction(lengthscale=5, sigma=1), noise_variance=0.01)
+    gp.observeData(np.array([20,30,70, 50]), np.array([0.5,0.5,2,1]))
+    priorDistSample(gp)
